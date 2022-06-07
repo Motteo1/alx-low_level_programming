e9adf20a4edbc31cd0f933f7d50923c7e7ef10e2
Motteo1 <timothykirungu@gmail.com>
2022-06-07 12:22:50 -0700

    Functions and nested loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,80 +1,22 @@
 #ifndef main.h
 #define main.h
 
-/**
- * _putchar -writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and error is set appropriately.
- */
-
 int _putchar(char c);
 
-/**
- * print_alphabet - print lowercase alphabet
- */
 
 void print_alphabet(void);
 
-/**
- * print_alphabet_x10 - print lowercase alphabet ten times
- */
-
-
 void print_alphabet_x10(void);
-
-/**
- * _islower -chacks for lowercase
- * @c: the character to check
- * Return: int
- */
-
 
 int _islower(int c);
 
-/**
- * _isalpha - checks for alphabet character
- * @c: character to check
- * Return: 1 if lowercase or uppercase, 0 if not alphabet character
- */
-
 int _isalpha(int c);
-
-/**
- * _print_sign - print sign of number
- * @n: number to check
- * Return: 1 if greater than 0, 0 if 0, -1 if less than 0
- */
 
 int print_sign(int n);
 
-/**
- * _abs - computer absolute value of integer
- * @int: integer
- * Return: absolute value
- */
-
 int _abs(int);
 
-/**
- * print_last_digit - print last digit of integer
- * @int: integer
- * Return: last digit or number
- */
-
 int print_last_digit(int);
-
-/**
- * jack_bauer - print every minute of a day
- * Description: print minutes 00:00 to 23:59
- */
-
-void jack_bauer(void);
-
-/**
- * time_table - print multiplication table
- */
 
 void times_table(void);
 
