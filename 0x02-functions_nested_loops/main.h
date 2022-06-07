c88fd4baae7c18e7a4675b29a3bfdbee8919fb2e
Motteo1 <timothykirungu@gmail.com>
2022-06-07 12:30:03 -0700

    Functions and nested loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,14 +1,8 @@
-#ifndef main.h
-#define main.h
-
 int _putchar(char c);
-
 
 void print_alphabet(void);
 
 void print_alphabet_x10(void);
-
-int _islower(int c);
 
 int _isalpha(int c);
 
@@ -18,27 +12,11 @@ int _abs(int);
 
 int print_last_digit(int);
 
-void times_table(void);
+void jack_bauer(void);
 
-/**
- * add - add two integers
- * @a: first integer
- * @b: second integer
- * Return: sum
- */
+void times_table(void);
 
 int add(int a, int b);
 
-/**
- * print_to_98 - print numbers n to 98
- * @n: integer argument
- */
-
 void print_to_98(int n);
 
-/**
- * print_times_table - print multiplication table up to n
- * @n: integer argument
- */
-
-#endif
