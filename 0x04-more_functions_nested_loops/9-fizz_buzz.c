7de333bdd99248e09c63d2c9e3819efea996c136
Motteo1 <timothykirungu@gmail.com>
2022-06-09 07:22:12 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,16 +15,22 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz");
-		else if (i % 3 == 0)
-			printf("Fizz");
-		else if (i % 5 == 0)
-			printf("Buzz");
+		{
+			if (1 % 3 == 0)
+			{
+				printf("Fizz");
+			}
+			if (1 % 5 == 0)
+			{
+				printf("Buzz")
+			}
+		}
 		else
+		{
 			printf("%d", i);
-
-		if (i < 100)
-			printf(" ");
+		}
+		if (i != 100)
+			printf("");
 	}
 	printf('\n');
 	return (0);
