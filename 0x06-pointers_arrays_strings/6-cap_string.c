5f16ce1d862c10718b2b6ce8983aad52a2fc37c4
Motteo1 <timothykirungu@gmail.com>
2022-06-16 15:20:35 -0700

    line after init

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- *cap_string - capitalize first letter of each word
- *@s: string to manipulate
- *Return: string
+ * cap_string - capitalize first letter of each word
+ * @s: string to manipulate
+ * Return: string
  */
 
 char *cap_string(char *s)
@@ -17,6 +17,7 @@ char *cap_string(char *s)
 
 	while (s[i] != '\0') /* iterate through string */
 	{
+		
 		/* if lowercase and prior char is separator, capitalize */
 		if ((s[i] >= 'a' && s[i] <= 'z')
 			&& (s[i - 1] == '-' || s[i - 1] == ';' || s[i - 1] == '.' ||
@@ -27,7 +28,6 @@ char *cap_string(char *s)
 			s[i] = s[i] - 'a' + 'A';
 		i++;
 	}
-
 
 	return (s);
 }
