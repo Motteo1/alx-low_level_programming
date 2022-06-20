791824bb9f4931add786e37e0b673458817e334c
Motteo1 <timothykirungu@gmail.com>
2022-06-20 10:19:08 -0700

    Initial commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - locate 1st occurence of char in string and returns pointers there
+ * @s: string search
+ * @c: target character
+ * Return: pointer to that character in string
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0' && s[i] != c) /* find match */
+		i++;
+
+	if (s[i] == c) /* if match, assign to address */
+		return (&s[i]);
+	else
+		return (NULL);
+}
