c8a02829e9ee4c6a94b292d38c6ea398ae26cb7a
Motteo1 <timothykirungu@gmail.com>
2022-06-22 15:47:17 -0700

    Fixing betty issues

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -4,7 +4,7 @@
  * substring_match - check if a substring after wildcard matches s1
  * @s1: one string
  * @s2: one string
- * @after_wild: place holder for position right after wildcard
+ * @after_wldcd: place holder for position right after wildcard
  * Return: 1 if matched, 0 if not
  */
 
@@ -13,11 +13,11 @@ int substring_match(char *s1, char *s2, char *after_wldcd)
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
 	if (*s1 == '\0' && *s2 == '*')
-		return (substring_match(s1, s2 + 1, s2 +1));
+		return (substring_match(s1, s2 + 1, s2 + 1));
 	if (*s1 == '\0' && *s2 != '\0')
 		return (0);
 	if (*s2 == '*')
-		return (substring_match(s1, s2 + 1, s2 +1));
+		return (substring_match(s1, s2 + 1, s2 + 1));
 	if (*s1 == *s2)
 		return (substring_match(s1 + 1, s2 + 1, after_wldcd));
 	else
