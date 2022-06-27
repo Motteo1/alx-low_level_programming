76063c0a204070a308727ef04a546dc6d0701412
Motteo1 <timothykirungu@gmail.com>
2022-06-27 11:40:56 -0700

    Fixed betty style

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -3,7 +3,7 @@
 #include "main.h"
 
 /**
- * free_grid frees a 2 dimensional grid
+ * free_grid - frees a 2 dimensional grid
  * previously created by your alloc_grid function
  * @grid: pointer to 2D array
  * @height: height of array
