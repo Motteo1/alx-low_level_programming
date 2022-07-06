3d3d1cde768c5d56b7a7c0d78311d632034c3ad8
Motteo1 <105060159+Motteo1@users.noreply.github.com>
2022-07-07 02:28:07 +0300

    fix header error

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,7 +8,7 @@
 * @action:pointer to function that we need to execute
 */
 
-void array_iterator(int *array, size_t size, void (*action)(int)
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t; /* match data type */
 	
