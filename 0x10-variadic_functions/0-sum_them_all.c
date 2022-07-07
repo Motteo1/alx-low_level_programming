5d8769db3499356f8e6c5771c8907a3db63444df
Motteo1 <timothykirungu@gmail.com>
2022-07-07 02:40:41 -0700

    fix valist

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,7 +15,7 @@ int sum_them_all(const unsigned int n, ...)
 	/* validate valist and initialize */
 	if (n == 0)
 		return (0);
-	va_start(va_list, n);
+	va_start(valist, n);
 
 	/* iterate through list, update sum, free list */
 	for (i = 0; i < n; i++)
