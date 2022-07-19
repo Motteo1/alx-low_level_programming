dedc774004a6627f3b7499b0229d1b34c921bd23
Motteo1 <timothykirungu@gmail.com>
2022-07-19 13:09:50 -0700

    Fix typos

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -7,7 +7,7 @@
  * Return: address of new element, or NULL if failed
  */
 
-listint_t *add_nodeint_end(listint_t **head, const in n)
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 
 	listint_t *new_node, *tmp;
