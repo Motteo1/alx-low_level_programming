094a4c8c0b6315542053421a855ac5bb304ea67f
Motteo1 <timothykirungu@gmail.com>
2022-07-21 05:03:49 -0700

    Initial commit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - convert binary to unsigned int
+ * @b: binary
+ * Return: unsigned int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+
+	int len = 0, i;
+	unsigned int sum = 0;
+
+	if (
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+~ Contains descriptions for the files in the current directory in regards to bit manipulation in C
