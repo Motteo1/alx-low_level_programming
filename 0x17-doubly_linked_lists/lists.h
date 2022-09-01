5cd1f1c162d6fb9f29063f3c1550e4ee9851aaa0
Motteo1 <timothykirungu@gmail.com>
2022-09-01 12:29:05 -0700

    Update

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -23,7 +23,7 @@ typedef struct dlistint_s
 /* function prototypes */
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
-dlistint_t *add_dnodeint(dlistint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 
 
 
