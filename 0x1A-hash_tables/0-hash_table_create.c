6c9449c7b8434f2edf3f6b59af722472acfa91c3
Motteo1 <timothykirungu@gmail.com>
2022-09-23 06:29:32 -0700

    Init commit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -5,3 +5,32 @@
  * @size: size
  * Return: pointer to table; NULL if error
  */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+	hash_node_t **array;
+	unsigned long int i = 0;
+
+	if (size == 0)
+		return (NULL);
+
+	ht = malloc(sizeof(hash_table_t));
+	if (!ht)
+		return (NULL);
+	array = malloc(sizeof(*array) * size);
+	if (!array)
+	{
+		free(ht);
+		return (NULL);
+	}
+	while (i < size)
+	{
+		array[1] = NULL;
+		i++;
+	}
+
+	ht->size = size;
+	ht->array = array;
+
+	return (ht);
+}
