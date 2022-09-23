9afefa85746ae8cc951f53f1b71a6cb3a6bd40af
Motteo1 <timothykirungu@gmail.com>
2022-09-23 06:39:57 -0700

    Init commit

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -85,5 +85,7 @@ shash_table_t *shash_table_create(unsigned long int size);
 int shash_table_set(shash_table_t *ht, const char *key, const char *value);
 char *shash_table_get(const shash_table_t *ht, const char *key);
 void shash_table_print(const shash_table_t *ht);
+void shash_tables_print_rev(const shash_table_t *ht);
+void shash_table_delete(shash(shash_table_t *ht);
 
 #endif
